9aae29f30ff222ce2c8f178fe9906ac01957ab0f
Ryan Tillaman <ryantillaman26@gmail.com>
2023-05-28 11:34:27 +0800

    feat: Added nominee card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import Button from "./components/Button";
-
+import { Fragment } from "react";
+import Nominee from "./components/Nominee";
+import data from "./data/data.json";
 function App() {
   return (
     <>
       <p>Online Votes</p>
-      <Button>Vote</Button>
+      {data?.map(({ id, title, nominees }) => {
+        return (
+          <Fragment key={id}>
+            <h1>{title}</h1>
+            {nominees?.map(({ id: nomineeId, image, name }) => {
+              return <Nominee key={nomineeId} name={name} image={image} />;
+            })}
+          </Fragment>
+        );
+      })}
     </>
   );
 }
--- src/components/Nominee/index.tsx
+++ src/components/Nominee/index.tsx
@@ -1,8 +1,27 @@
-import React from "react";
 import styles from "./styles.module.css";
+import Button from "../Button";
 
-const Nominee = () => {
-  return <div>Nominee</div>;
+interface Props {
+  name: string;
+  image: string;
+  voted?: boolean;
+}
+const Nominee = ({ name, image, voted = false }: Props) => {
+  return (
+    <article className={`${styles.card}`}>
+      <header className={`${styles.header}`}>{name}</header>
+      <main>
+        <img
+          className={`${styles.image}`}
+          src={`nominees/gentlemen/${image}`}
+          alt={name}
+        />
+      </main>
+      <footer>
+        <Button bgColor={voted ? "yellow" : "gray"}>Vote</Button>
+      </footer>
+    </article>
+  );
 };
 
 export default Nominee;
